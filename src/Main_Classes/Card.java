fe5da43dcf54234479d3acca123ac6ae7a0380b8
Erlurdor <rafaelgolovin@mail.ru>
2019-06-30 13:44:29 +0700

    Добвлены классы: Игрок, Карта

---
+++ src/Main_Classes/Card.java
@@ -0,0 +1,13 @@
+package Main_Classes;
+
+public class Card {
+    public void Walk_At_Day()
+    {
+
+    };
+
+    public void Walk_At_Night()
+    {
+
+    }
+}
---
+++ src/Main_Classes/Player.java
@@ -0,0 +1,13 @@
+package Main_Classes;
+
+//import javax.smartcardio.Card;
+//import Main_Classes.Card;
+
+//  Основной класс игрока. Содержит характеристики.
+
+public class Player {
+    private int Lick;       //показатель удачи игрока
+    private String Name;         //имя игрока
+    private boolean Died;       //жив ли игрок. False - живой
+    private Card Role;          //карточка с ролью для игрока
+}
